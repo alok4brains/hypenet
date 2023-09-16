6ad8bedbcc28e2fd89b04afbbe6696d6b6bf26a5
alok@4brains.in <alok@4brains.in>
2023-09-16 09:32:18 +0530

    new updated

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // This exportPathMap configuration assumes that you have
-  // pages like index.js, about.js, etc.
-  // Adjust as necessary for your page structure.
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    if (dev) {
-      return defaultPathMap;
-    }
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-      "/services": { page: "/services" },
-      "/blogs": { page: "/blogs" },
-      "/blogs-content": { page: "/blogs-content" },
-      "/careers": { page: "/careers" },
-      // Add other routes here as necessary
-    };
-  },
 };
 
 module.exports = nextConfig;
